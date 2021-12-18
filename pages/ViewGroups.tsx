0a75f82878e5fea8a0159ac88e101e48eb6c259e
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-12-18 19:31:06 +0000

    added hook to ViewGroup

--- pages/ViewGroups.tsx
+++ pages/ViewGroups.tsx
@@ -1,10 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import React, { FC } from "react";
+import { Text, View, FlatList } from "react-native";
+import { useViewGroups } from "../hooks";
+import { Group } from "../types";
+
+const Item: FC<Group> = ({ name, category, summary }) => {
+  return (
+    <View>
+      <Text>{name}</Text>
+      <Text>{category}</Text>
+      <Text>{summary}</Text>
+    </View>
+  );
+};
 
 const ViewGroups = () => {
+  const { groups } = useViewGroups();
+
   return (
     <View>
       <Text>View Groups</Text>
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => <Item {...item} />}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };

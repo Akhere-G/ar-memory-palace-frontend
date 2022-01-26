eff5e96598e5cf488dd5d42f21445e988f889be2
Akhere Ihoeghinlan <101akhere5@gmail.com>
2022-01-26 18:50:52 +0000

    added view saved group functionality

--- hooks.tsx
+++ hooks.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
+import { Platform } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import jwt_decode from "jwt-decode";
 import * as api from "./api";
-import { Group } from "./types";
-import { CreateGroupData, SignIntoGroupData } from "./types";
+import { Group, CreateGroupData, SignIntoGroupData } from "./types";
 import {
   validateCreateGroupData,
   validateSignIntoGroupData,
@@ -20,7 +20,12 @@ type GetGroupTokens = () => Promise<TokenList>;
 
 export const getGroupTokens: GetGroupTokens = async () => {
   try {
-    const exisitingTokenList = await SecureStore.getItemAsync(storageKey);
+    let exisitingTokenList;
+    if (Platform.OS === "web") {
+      exisitingTokenList = localStorage.getItem(storageKey);
+    } else {
+      exisitingTokenList = await SecureStore.getItemAsync(storageKey);
+    }
     if (exisitingTokenList) {
       const tokens = (await JSON.parse(exisitingTokenList)) as TokenList;
       return tokens;
@@ -36,39 +41,42 @@ export const storeGroupToken = async (token: string) => {
     const { id } = jwt_decode(token) as any;
     let tokens: TokenList = await getGroupTokens();
     tokens[id] = token;
-    await SecureStore.setItemAsync(storageKey, token);
+    if (Platform.OS === "web") {
+      localStorage.setItem(storageKey, JSON.stringify(tokens));
+    } else {
+      await SecureStore.setItemAsync(storageKey, JSON.stringify(tokens));
+    }
   } catch (err: any) {
     console.log(err);
   }
 };
 
-export const useFetchGroups = () => {
+export const useGetGroups = () => {
   const [groups, setGroups] = useState<Group[]>([]);
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
-  useEffect(() => {
-    const fetchGroups = async () => {
-      try {
-        setLoading(true);
-        const groupTokens = await getGroupTokens();
-        const newGroups: Group[] = Object.values(groupTokens).map((token) =>
-          jwt_decode(token)
-        );
-        setGroups(newGroups);
-        setTotal(newGroups.length);
-        setLoading(false);
-        setError("");
-      } catch (err: any) {
-        setLoading(false);
-        setError(err);
-      }
-    };
-    fetchGroups();
-  }, []);
+  const getGroups = async () => {
+    try {
+      setLoading(true);
+      setError("");
 
-  return { groups, total, loading, error };
+      const groupTokens = await getGroupTokens();
+      const newGroups: Group[] = Object.values(groupTokens).map((token) =>
+        jwt_decode(token)
+      );
+      setGroups(newGroups);
+      setTotal(newGroups.length);
+      setLoading(false);
+      setError("");
+    } catch (err: any) {
+      setLoading(false);
+      setError(err);
+    }
+  };
+
+  return { groups, total, loading, error, getGroups };
 };
 
 export const useSignIntoGroup = (signIntoGroupAPI = api.signIntoGroup) => {
--- pages/ViewGroups.tsx
+++ pages/ViewGroups.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from "react";
-import { Text, View, FlatList, Button, StyleSheet } from "react-native";
-import { useFetchGroups } from "../hooks";
+import React, { FC, useEffect } from "react";
+import {
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Button,
+  StyleSheet,
+} from "react-native";
+import { useGetGroups } from "../hooks";
 import { Group } from "../types";
 
 const Item: FC<Group> = ({ name, category, summary }) => {
   return (
-    <View>
-      <Text>{name}</Text>
+    <View style={styles.GroupItem}>
+      <Text style={styles.Title}>{name}</Text>
       <Text>{category}</Text>
       <Text>{summary}</Text>
     </View>
@@ -14,29 +21,33 @@ const Item: FC<Group> = ({ name, category, summary }) => {
 };
 
 const ViewGroups = (props: any) => {
-  const { groups, loading, error } = useFetchGroups();
+  const { groups, loading, error, getGroups } = useGetGroups();
   const { navigation } = props;
+
+  useEffect(() => {
+    getGroups();
+  }, []);
 
   if (loading) {
     return (
-      <View>
-        <Text>Loading...</Text>
+      <View style={styles.Main}>
+        <Text style={styles.Title}>Loading...</Text>
       </View>
     );
   }
 
   if (error) {
     return (
-      <View>
-        <Text>Error... {error}</Text>
+      <View style={styles.Main}>
+        <Text style={styles.Title}>Error... {error}</Text>
       </View>
     );
   }
 
   if (groups.length === 0) {
     return (
-      <View>
-        <Text>No groups</Text>
+      <View style={styles.Main}>
+        <Text style={styles.Title}>No groups</Text>
         <View style={styles.Button}>
           <Button
             title="You have no groups... Create a new group?"
@@ -54,22 +65,50 @@ const ViewGroups = (props: any) => {
   }
 
   return (
-    <View>
-      <Text>View Groups</Text>
+    <ScrollView style={styles.Main}>
       <FlatList
         data={groups}
         renderItem={({ item }) => <Item {...item} />}
         keyExtractor={(item) => item.id}
       />
-    </View>
+      <View style={styles.Button}>
+        <Button
+          title="You have no groups... Create a new group?"
+          onPress={() => navigation.navigate("CreateGroup")}
+        />
+      </View>
+      <View style={styles.Button}>
+        <Button
+          title="You have no groups... sign into a new group?"
+          onPress={() => navigation.navigate("SignIntoGroup")}
+        />
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
+  Main: {
+    padding: 10,
+  },
+  Title: {
+    paddingBottom: 10,
+    fontSize: 20,
+  },
+  GroupItem: {
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+    borderColor: "#aaa",
+    borderStyle: "solid",
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    backgroundColor: "white",
+    padding: 10,
+  },
   Button: {
     paddingTop: 10,
-    marginLeft: "auto",
-    marginRight: "auto",
-    maxWidth: "60%",
+    paddingLeft: 10,
+    paddingRight: 10,
   },
 });
 
